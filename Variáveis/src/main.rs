70ac220305469d6270972cececfc057e4a82117e
Beto Rocha <roberto.blockchainresources@gmail.com>
2025-02-21 11:54:13 -0300

    oi

--- Variáveis/src/main.rs
+++ Variáveis/src/main.rs
@@ -1,21 +1,11 @@
-//! # Os tipos s'ao seus amigos mano
-//! 
-//! Rust [e uma linguagem de programa;'ao com tipagem estatica e forte, o que significa que:
-//! - Tipagem estática: O tipo de cada variável é determinado em tempo de compilação e não muda.
-//! Uma variável declarada com um tipo específico não pode ser restribuída a um valor de outro tipo sem uma conversão e
-//! Tipagem forte: Rust é rigoroso e uma string sem converter explicitamente um deles para o tipo compatível o outro.
-//! 
-//! O compilador do Rust é inteligente o suficiente para inferir o tipo de muitas variáveis.
-//! O que significa que nem sempre é necessário especificar o tipo explicitamente.
-//! 
-//! Nest aula, focaremos nos tipos primitivos, que são os blocos de construção básicos para a criação de programas em
-
-
-
 fn main() {
-
-    println!("{}", std::mem::size_of::<i8>());
-    println!("{}", std::mem::size_of::<(i32, i8, i8)>());
-
-
+    let age: i32 = 9;
+    let mut result: bool = age > 18;
+    result = age < 18;
+    result = age >=
+    if result > 18 {
+        println!("Qual cerveja você vai querer?");
+    } else {
+        println!("Nao pode")
+    }
 }
